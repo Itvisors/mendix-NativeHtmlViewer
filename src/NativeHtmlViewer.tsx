0282e3dd7067f4d276582f37677a8bcaf77b1c1d
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2021-10-10 16:03:39 +0200

    Hardcoded default text color

--- src/NativeHtmlViewer.tsx
+++ src/NativeHtmlViewer.tsx
@@ -1,18 +1,24 @@
 import { Component, ReactNode, createElement } from "react";
-import { TextStyle, ViewStyle } from "react-native";
+import { TextStyle } from "react-native";
 
-import { Style } from "@mendix/pluggable-widgets-tools";
+import { mergeNativeStyles, Style } from "@mendix/pluggable-widgets-tools";
 
 import { NativeHtmlViewerProps } from "../typings/NativeHtmlViewerProps";
 import { NativeHtmlRenderer } from "./components/NativeHtmlRenderer";
 
 export interface CustomStyle extends Style {
-    container: ViewStyle;
-    label: TextStyle;
+    body: TextStyle;
 }
+
+const defaultStyle: CustomStyle = {
+    body: {
+        color: "#E7E7E9" // Default text color
+    }
+};
 
 export class NativeHtmlViewer extends Component<NativeHtmlViewerProps<CustomStyle>> {
     render(): ReactNode {
-        return <NativeHtmlRenderer content={this.props.content?.value}></NativeHtmlRenderer>;
+        const styles = mergeNativeStyles(defaultStyle, this.props.style);
+        return <NativeHtmlRenderer content={this.props.content?.value} styles={styles}></NativeHtmlRenderer>;
     }
 }
--- src/components/NativeHtmlRenderer.tsx
+++ src/components/NativeHtmlRenderer.tsx
@@ -1,9 +1,11 @@
 import { createElement, ReactElement } from "react";
 import { useWindowDimensions } from "react-native";
 import RenderHtml from "react-native-render-html";
+import { CustomStyle } from "../NativeHtmlViewer";
 
 export interface NativeHtmlRendererProps {
     content?: string;
+    styles: CustomStyle;
 }
 
 export const NativeHtmlRenderer = (props: NativeHtmlRendererProps): ReactElement => {
@@ -11,5 +13,5 @@ export const NativeHtmlRenderer = (props: NativeHtmlRendererProps): ReactElement
     const source = {
         html: props.content ? props.content : ""
     };
-    return <RenderHtml contentWidth={width} source={source} />;
+    return <RenderHtml contentWidth={width} source={source} tagsStyles={{ body: { color: "#E7E7E9" } }} />;
 };
