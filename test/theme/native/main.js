31d1d7ceaf604f3c12c36c227762f04aa16ea9e7
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2024-12-30 14:13:27 +0100

    Place styling in test module

--- test/theme/native/main.js
+++ test/theme/native/main.js
@@ -1,3 +1,2 @@
 export {};
 
-export * from "./testNativeHtmlViewer";
--- test/themesource/testnativehtmlviewer/native/main.js
+++ test/themesource/testnativehtmlviewer/native/main.js
@@ -1,2 +1,3 @@
 import * as variables from "../../../theme/native/custom-variables";
 
+export * from "./testNativeHtmlViewer";
